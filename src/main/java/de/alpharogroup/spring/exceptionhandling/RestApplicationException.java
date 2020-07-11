12c77caa24db6e4faeab16fb8e848ad34d36e417
astrapi69 <asterios.raptis@gmx.net>
2020-07-11 12:36:33 +0200

    new RestApplicationException created that extends RuntimeException

--- src/main/java/de/alpharogroup/spring/exceptionhandling/RestApplicationException.java
+++ src/main/java/de/alpharogroup/spring/exceptionhandling/RestApplicationException.java
@@ -3,27 +3,21 @@ package de.alpharogroup.spring.exceptionhandling;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 /**
  * The general class {@link RestApplicationException} for rest controller exceptions
  */
-@Getter @NoArgsConstructor @AllArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) public class RestApplicationException
+@Getter @AllArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) public class RestApplicationException
 	extends RuntimeException
 {
 
 	int code;
-	String message;
+	String errorMessage;
 
 	public RestApplicationException(int code)
 	{
 		this.code = code;
-	}
-
-	public RestApplicationException(String message)
-	{
-		this.message = message;
 	}
 
 }
