8169a39f3f97b3f245532999dfaff4634db58bf3
astrapi69 <asterios.raptis@gmx.net>
2020-07-15 18:18:31 +0200

    new class AutowireAware created for autowire classes like jpa listeners

---
+++ src/main/java/de/alpharogroup/spring/autowire/AutowireAware.java
@@ -0,0 +1,37 @@
+package de.alpharogroup.spring.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Utility class for autowire classes like in jpa listeners
+ */
+@Component
+public final class AutowireAware implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    private AutowireAware() {
+    }
+
+    /**
+     * Try to autowire the given object that contains the bean instances which need to be autowired
+     *
+     * @param classHolderToAutowire  the holder instance of the class which holds @Autowire annotations
+     * @param beansToAutowireInClass the beans which have the @Autowire annotation in the specified {#classHolderToAutowire}
+     */
+    public static void autowire(Object classHolderToAutowire, Object... beansToAutowireInClass) {
+        for (Object bean : beansToAutowireInClass) {
+            if (bean == null) {
+                applicationContext.getAutowireCapableBeanFactory().autowireBean(classHolderToAutowire);
+            }
+        }
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) {
+        AutowireAware.applicationContext = applicationContext;
+    }
+
+}
